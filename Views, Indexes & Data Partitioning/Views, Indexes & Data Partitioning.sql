bf242568d7b1a501b9d7a36a27df504ccfb2e153
Ankit Tomar <ankitjdec@gmail.com>
2025-04-05 21:37:23 +0530

    created children view from customer table

---
+++ Views, Indexes & Data Partitioning/Views, Indexes & Data Partitioning.sql
@@ -0,0 +1,17 @@
+/*
+Views:-
+is a database object that has no values.
+Its contents are based on the base table.
+It is a virtual table created by a query
+
+simplify complex query
+increase the reusability
+help in data security
+enable backward compatibility
+*/
+
+create view `children` as
+select customerkey, firstname,lastname,totalchildren
+from customers;
+
+select * from children;
