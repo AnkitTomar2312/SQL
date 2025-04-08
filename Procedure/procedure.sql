c2a1495c240a9b3e0c1fc4fb462376e98ecb966a
Ankit Tomar <ankitjdec@gmail.com>
2025-04-08 22:04:20 +0530

    procedure begin

---
+++ Procedure/procedure.sql
@@ -0,0 +1,43 @@
+use coding_ninja;
+
+CREATE TABLE student_info (
+
+    stud_id INT PRIMARY KEY,
+
+    stud_code INT,
+
+    stud_name VARCHAR(50),
+
+    subject VARCHAR(50),
+
+    marks INT,
+
+    phone VARCHAR(15)
+
+);
+
+-- Insert data
+
+INSERT INTO student_info (stud_id, stud_code, stud_name, subject, marks, phone) VALUES 
+
+(1, 101, 'Mark', 'English', 68, '3454569357'),
+
+(2, 102, 'Joseph', 'Physics', 70, '9876543659'),
+
+(3, 103, 'John', 'Maths', 70, '9765326975'),
+
+(4, 104, 'Barack', 'Maths', 92, '87069873256'),
+
+(5, 105, 'Rinky', 'Maths', 85, '6753159757'),
+
+(6, 106, 'Adam', 'Science', 82, '79642256864'),
+
+(7, 107, 'Andrew', 'Science', 83, '5674243579'),
+
+(8, 108, 'Brayan', 'Science', 83, '7524316576'),
+
+(9, 109, 'Alexandar', 'Biology', 67, '2347346438');
+
+select * from student_info;
+
+-- procedure without any parameter
