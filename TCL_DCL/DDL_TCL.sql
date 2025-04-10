48b51d1e95cae4a675a3464746479878265b710f
Ankit Tomar <ankitjdec@gmail.com>
2025-04-10 23:00:07 +0530

    completed

--- TCL_DCL/DDL_TCL.sql
+++ TCL_DCL/DDL_TCL.sql
@@ -41,7 +41,175 @@ select * from employee1;
 -- savepoint concept begin
 -- we divide database into parts and we can save the parts of different transaction using save point
 
+START TRANSACTION;
+
+ 
+
+UPDATE employee1
+
+SET occupation = 'Teacher'
+
+where name = 'Warner';
+
+ 
+
+select * from employee1;
+
+ 
+
+SAVEPOINT second_update;
+
+ 
+
+INSERT INTO employee1 VALUES    
+
+('Victor', 'Player', '2020-10-18', 15);
+
+ 
+
+select * from employee1;
+
+ 
+
+SAVEPOINT second_insert;
+
+ 
+
+select * from employee1;
+
+ 
+
+ROLLBACK TO SAVEPOINT second_update;
+
+ 
+
+select * from employee1;
+
+ 
+
+COMMIT;
+
+ 
+
+ROLLBACK;
+
+  
+
+-- Update working hours
+
+UPDATE employee1
+
+SET working_hours = working_hours * 1.10;
+
+ 
+
+select * from employee1;
+
+ 
+
+-- Insert a new customer
+
+INSERT INTO employee1 VALUES    
+
+('Markus', 'Farmer', '2020-10-08', 14);
+
+ 
+
+select * from employee1;
+
+ 
+
+COMMIT;
+
+ 
+
+select * from employee1;
+
+ 
+
+Rollback;
+
+ 
+
+select * from employee1;
+
+START TRANSACTION;
+
+ 
+
+UPDATE employee1
+
+SET occupation = 'Data Scientist'
+
+where name = 'Robin';
+
+ 
+
+select * from employee1;
+
+ 
+
+SAVEPOINT first_update;
+
+ 
+
+INSERT INTO employee1 VALUES    
+
+('Alex', 'Actor', '2020-10-10', 13);
+
+ 
+
+SAVEPOINT first_insert;
+
+ 
+
+COMMIT;
+
+ 
+
+select * from employee1;
+
+select * from employee1;
+
 -- DCL command begin
 /*
-1. Grant Command:- 
+1. Grant Command:- to give the access
+
+grant priviledge_name
+on object_name
+to {user|public|role_name}
+[with grant option]
+
+2. Revoke Command:- to take back the access
 */
+
+-- Generic Syntax
+
+-- GRANT privilege_name
+
+-- ON object_name
+
+-- TO {user_name |PUBLIC |role_name}
+
+-- [WITH GRANT OPTION];
+
+ 
+
+-- Example
+
+ 
+
+-- GRANT SELECT (Productname, productprice), 
+
+-- INSERT (productSKU, Productname) 
+
+-- ON mydatabase.products 
+
+-- TO 'user4'@'localhost';
+
+-- REVOKE Command - Used to revoke certain access.
+
+ 
+
+-- REVOKE SELECT ON student FROM 'user4'@'localhost';
+
