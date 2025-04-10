c8763f214f8f82b8a9e2fe33738fabb45b4ec7c8
Ankit Tomar <ankitjdec@gmail.com>
2025-04-10 21:49:06 +0530

    other triggers created

--- triggers/trigger.sql
+++ triggers/trigger.sql
@@ -77,3 +77,75 @@ BEGIN
 END //
 
 DELIMITER ;
+
+CREATE TABLE employee(  
+
+    name varchar(45) NOT NULL,    
+
+    occupation varchar(35) NOT NULL,    
+
+    working_date date,  
+
+    working_hours varchar(10)  
+
+);
+
+INSERT INTO employee VALUES    
+
+('Robin', 'Scientist', '2020-10-04', 12),  
+
+('Warner', 'Engineer', '2020-10-04', 10),  
+
+('Peter', 'Actor', '2020-10-04', 13),  
+
+('Marco', 'Doctor', '2020-10-04', 14),  
+
+('Brayden', 'Teacher', '2020-10-04', 12),  
+
+('Antonio', 'Business', '2020-10-04', 11);
+
+DELIMITER //
+
+CREATE TRIGGER before_delete_employee
+
+BEFORE DELETE ON employee
+
+FOR EACH ROW
+
+BEGIN
+
+    IF OLD.occupation = 'Actor' THEN
+
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Actors data can not be deleted';
+
+    END IF;
+
+END //
+
+DELIMITER ;
+
+DELETE FROM employee WHERE occupation = 'Actor';
+
+DELIMITER //
+
+CREATE TRIGGER before_update_employee
+
+BEFORE UPDATE ON employee
+
+FOR EACH ROW
+
+BEGIN
+
+    IF OLD.name <> 'Peter' THEN
+
+        SET NEW.working_hours = NEW.working_hours; 
+
+    ELSEIF OLD.name = 'Peter' THEN
+
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Update not allowed for Peter';
+
+    END IF;
+
+END //  
+
+DELIMITER ;
