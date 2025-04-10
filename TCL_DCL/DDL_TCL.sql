184bcc528a832e106c5e5f201386b5227a719b55
Ankit Tomar <ankitjdec@gmail.com>
2025-04-10 22:06:38 +0530

    created a new table

--- TCL_DCL/DDL_TCL.sql
+++ TCL_DCL/DDL_TCL.sql
@@ -3,3 +3,28 @@ Transaction- work performed againt database in sql
 commit- permanently save all the transaction 
 */
 
+CREATE TABLE employee1(  
+
+    name varchar(45) NOT NULL,    
+
+    occupation varchar(35) NOT NULL,    
+
+    working_date date,  
+
+    working_hours varchar(10)  
+
+);
+
+INSERT INTO employee1 VALUES    
+
+('Robin', 'Scientist', '2020-10-04', 12),  
+
+('Warner', 'Engineer', '2020-10-04', 10),  
+
+('Peter', 'Actor', '2020-10-04', 13),  
+
+('Marco', 'Doctor', '2020-10-04', 14),  
+
+('Brayden', 'Teacher', '2020-10-04', 12),  
+
+('Antonio', 'Business', '2020-10-04', 11);
